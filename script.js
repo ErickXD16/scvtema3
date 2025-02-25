76c3bebf01a880f764e32e71f20160296477e461
ErickMora <soyerickxd16@gmail.com>
2025-02-25 15:37:32 -0600

    Se elimino el archivo index2.html

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Página cargada completamente');
+});
+
+document.getElementById('clickButton').addEventListener('click', function() {
+    alert('¡Botón clicado!');
+});
